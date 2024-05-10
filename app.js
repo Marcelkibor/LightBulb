f8e4fbd6ee938ae33351c042ca5622d6fcd160de
Marcelkibor <kibormarcel90@gmail.com>
2024-05-10 16:25:44 +0300

    draggable string setup

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('connected');
+function drag(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text/plain");
+    var draggedElement = document.getElementById(data);
+    var container = document.querySelector(".container");
+    var containerRect = container.getBoundingClientRect();
+
+    var offsetY = event.clientY - containerRect.top;
+    var maxOffsetY = containerRect.height - draggedElement.clientHeight;
+    var newY = Math.min(maxOffsetY, Math.max(0, offsetY));
+
+    draggedElement.style.bottom = (containerRect.height - newY) + "px";
+});
