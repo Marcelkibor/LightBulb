8ada594e4fd12ab1e6f994490cb5a376107f28b1
Marcelkibor <kibormarcel90@gmail.com>
2024-05-23 09:04:31 +0300

    Refactor

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 document.addEventListener('DOMContentLoaded',function(){
-    let link = document.getElementById('demo-bulb');
-    let line = document.getElementById('line');
-    let bulbStyles = {
-      a:'custom-icons',
-      b:'clicked'
-    }
-    if(link){
-      line.addEventListener('click',()=>{
-       let res = handleColorState();
-       if(res.length==1){
-        line.style.backgroundColor="black";
-       }
+    let bulb = document.getElementById('demo-bulb');
+    let bulbLine = document.getElementById('line');
+    if(bulb){
+      bulb.addEventListener('click',()=>{
+        bulb.classList.toggle('clicked');
+        bulbLine.classList.toggle('clicked');
+
       })
     }
-   const handleColorState=()=>{
-    link.classList.toggle('clicked');
-    if(line){
-        line.style.backgroundColor="rgb(228, 131, 4)"
-    }
-    return link.classList 
-  }
 
 });
