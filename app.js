2cfd59625b3d8c8585ce277b100cd86b8dc4eb10
Marcelkibor <kibormarcel90@gmail.com>
2024-05-25 22:22:48 +0300

    handle drag function

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 document.addEventListener('DOMContentLoaded',function(){
-    let bulb = document.getElementById('demo-bulb');
-    let bulbLine = document.getElementById('line');
-    let startY = 0;
-    let initialHeight = 0;
-    let dragging = false;
-    if(bulb){
-      bulb.addEventListener('click',()=>{
-        bulb.classList.toggle('clicked');
-        bulbLine.classList.toggle('clicked');
-      })
-    }
+let bulb = document.getElementById('demo-bulb');
+let bulbLine = document.getElementById('line');
+let dragging = false;
 
-    if (bulbLine) {
-      bulbLine.addEventListener('mousedown', (event) => {
-        console.log(startY,initialHeight,dragging,'before')
-        startY = event.clientY;
-        initialHeight = parseInt(window.getComputedStyle(bulbLine).height, 10);
-        dragging = true;
-        console.log(startY,initialHeight,dragging,'after')
-      });
-    }
-    document.addEventListener('mousemove', (event) => {
-      if (dragging) {
-        const currentY = event.clientY;
-        const deltaY = currentY - startY;
-        const newHeight = initialHeight + deltaY;
-        if (newHeight > 0) {
-          bulbLine.style.height = `${newHeight}px`;
-        }
-      }
-    });
-    document.addEventListener('mouseup', () => {
-      if (dragging) {
-        dragging = false;
-      }
-    });
+bulb.addEventListener('click',()=>{
+bulb.classList.toggle('clicked');
+bulbLine.classList.toggle('clicked');
+})
+
+const handleDrag =()=>{
+try{
+bulbLine.addEventListener('dragenter',(event)=>{
+event.preventDefault();
+dragging = true;
+console.log('Element dragged!');
+})
+}
+catch(error){
+console.log(`Error`,error);
+}
+
+}
+handleDrag();
 
 });
